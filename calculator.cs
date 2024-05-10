885a34e2bb4e194cc9ec27b929f565c5402491e3
kellyt1 <tossy.kelly@gmail.com>
2024-05-10 12:28:40 -0500

    add code to repo

---
+++ calculator.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class calculator
+    {
+        private readonly ILogger<calculator> _logger;
+
+        public calculator(ILogger<calculator> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("calculator")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
